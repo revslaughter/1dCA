cdbf0b3321a3c843f89051de0277bec80ec589e6
revslaughter <prairie.squidman@gmail.com>
2011-06-08 16:25:34 -0500

    added closed (or cylindrical) environment, and made it the default. Next on the list is going to be the ability to add/remove generations or to start from a generation and throw away the beginning, so that you can view a whole lifetime. It may also be fun to be able to increase more mutably the size of the playing field so to speak, but I dont know how to begin doing that.

--- pyelemCA.py
+++ pyelemCA.py
@@ -3,14 +3,24 @@
 # class elem is a matrix of lists of 1s and 0s with methods in it?
 
 class elem:
-    def __init__(self, rule, gen, initial, isring):
+    def __init__(self, rule, gen, initial, isclosed=1):
 	self.rule = self.ruleform(rule)
 	self.seed = initial
-	self.loop = isring
+	self.state = isclosed
 	builder = initial
 	self.matrix = []
 	self.matrix.append(builder)
-	if self.loop:
+	self.whatdo = {
+	    7 : self.rule[0],
+	    6 : self.rule[1],
+	    5 : self.rule[2],
+	    4 : self.rule[3],
+	    3 : self.rule[4],
+	    2 : self.rule[5],
+	    1 : self.rule[6],
+	    0 : self.rule[7]
+	}
+	if self.state:
 	    for x in range(0,gen):
 		builder = self.loopline(self.rule, builder)
 		self.matrix.append(builder)
@@ -45,20 +55,15 @@ class elem:
 	# I wanted to use lists of the form [1,1,1] but guess what, they're not
 	# to be used as dictionary keys. Mutable. I could use tuples, perhaps for
 	# clarity but this works too - just use the base-10 version of the codon
-	whatdo = {
-	    7 : binrule[0],
-	    6 : binrule[1],
-	    5 : binrule[2],
-	    4 : binrule[3],
-	    3 : binrule[4],
-	    2 : binrule[5],
-	    1 : binrule[6],
-	    0 : binrule[7]
-	}
 	newline.append(line[0]) #The ends never change
 	for x in range(1, len(line)-1):
-	    newline.append(whatdo[((4*line[x-1])+(2*line[x])+(line[x+1]))])
+	    newline.append(self.whatdo[((4*line[x-1])+(2*line[x])+(line[x+1]))])
 	newline.append(line[-1])
 	return newline
-
-    
+    def loopline(self, binrule, line):
+	newline = []
+	newline.append(self.whatdo[((4*line[-1])+(2*line[0])+(line[1]))])
+	for x in range(1, len(line)-1):
+	    newline.append(self.whatdo[((4*line[x-1])+(2*line[x])+(line[x+1]))])
+	newline.append(self.whatdo[((4*line[-2])+(2*line[-1])+(line[0]))])
+	return newline
