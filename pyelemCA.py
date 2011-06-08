dc83e25c7a8472311c495ffc9759bcd43df6bae4
revslaughter <prairie.squidman@gmail.com>
2011-06-07 23:24:49 -0500

    Got this in a working state. Need to add more methods to make it a little more flexible, but, yeah, it works and seems to work well. Still need to add 'ring', which means instead of a line, the next generation is computed where the ends 'wrap around' and affect each other. Maybe I should just take it out.

--- pyelemCA.py
+++ pyelemCA.py
@@ -3,7 +3,7 @@
 # class elem is a matrix of lists of 1s and 0s with methods in it?
 
 class elem:
-    def __init__(rule, gen=0, initial, isring=False):
+    def __init__(self, rule, gen, initial, isring):
 	self.rule = self.ruleform(rule)
 	self.seed = initial
 	self.loop = isring
@@ -19,10 +19,16 @@ class elem:
 		builder = self.nextline(self.rule, builder)
 		self.matrix.append(builder)
     def __repr__(self):
-	for x in self.matrix:
-	    #This will have to change based on what we have in self.nextline
-	    print ((x.replace('0', '_')).replace('1', '#') + " " + str(int("0b"+x, 2)))
-    def ruleform(rule):
+	builder = ""
+	for x in range(0, len(self.matrix)):
+	    for y in self.matrix[x]:
+		if y:
+		    builder += "#"
+		else:
+		    builder += "_"
+	    builder += "\n"
+	return builder
+    def ruleform(self, rule):
 	binaryform=[]
 	if rule in range(0, 256):
 	    quotient = 128
@@ -34,7 +40,25 @@ class elem:
 		    binaryform.append(0)
 		quotient = quotient/2
 	return binaryform
-    def nextline(rule, line):
-	
-    
+    def nextline(self, binrule, line):
+	newline = []
+	# I wanted to use lists of the form [1,1,1] but guess what, they're not
+	# to be used as dictionary keys. Mutable. I could use tuples, perhaps for
+	# clarity but this works too - just use the base-10 version of the codon
+	whatdo = {
+	    7 : binrule[0],
+	    6 : binrule[1],
+	    5 : binrule[2],
+	    4 : binrule[3],
+	    3 : binrule[4],
+	    2 : binrule[5],
+	    1 : binrule[6],
+	    0 : binrule[7]
+	}
+	newline.append(line[0]) #The ends never change
+	for x in range(1, len(line)-1):
+	    newline.append(whatdo[((4*line[x-1])+(2*line[x])+(line[x+1]))])
+	newline.append(line[-1])
+	return newline
+
     
